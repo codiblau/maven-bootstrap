386b01cbcdd8b4b7c50b24a547c222856c264e44
joangalmesriera <joan.galmes@gmail.com>
2021-12-16 16:47:47 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const { series } = require('gulp')
+const { series } = require('gulp');
+
+//function buildStyles() {
+//    return gulp.src('./node_modules/bootstrap/**/*.scss')
+//        .pipe(sass().on('error', sass.logError))
+//        .pipe(gulp.dest('./src/main/resources/static/css'));
+//}
 
 function buildStyles() {
-    return gulp.src('./src/**/*.scss')
+    return gulp.src('./src/main/resources/static/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/main/resources/static/css'));
 }
 
-exports.compila = buildStyles
+exports.compila = buildStyles;
